c20a581f5bc66c7a5aff1ba49e19fba6b1e320cf
Siddhipatade <patadesiddhi20@gmail.com>
2023-06-23 22:29:58 +0530

    initial commit

---
+++ code/selectionsort.py
@@ -0,0 +1,24 @@
+import sys
+
+# Input the array elements
+A = []
+n = int(input("Enter the number of elements: "))
+for i in range(n):
+    element = int(input("Enter element {}: ".format(i+1)))
+    A.append(element)
+
+# Traverse through all array elements
+for i in range(len(A)):
+    # Find the minimum element in the remaining unsorted array
+    min_idx = i
+    for j in range(i+1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+
+    # Swap the found minimum element with the first element	
+    A[i], A[min_idx] = A[min_idx], A[i]
+
+# Print the sorted array
+print("Sorted array:")
+for i in range(len(A)):
+    print(A[i], end=" ")
